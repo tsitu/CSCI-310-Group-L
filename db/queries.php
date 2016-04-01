c4009262c6002590c5840890d1d3d714ce4643cc
Steve Chang <steve.chang.1994@gmail.com>
2016-04-01 16:32:38 -0700

    added what we have so far...

--- db/queries.php
+++ db/queries.php
@@ -1,14 +1,15 @@
 <?php
 
-require_once __DIR__ . "/db/connect.php";
-require_once __DIR__ . "/data/Transaction.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/db/connect.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/data/Transaction.php";
 
 
 //Removes all transactions tied to $userId "AND" $accountId
 //"Remove Account" function on UI.
 function removeAccount($userId, $accountId) {
+	global $mysqli;
 	//prepare
-	if( ($stmt = $db->prepare("DELETE FROM transactions WHERE userId=? AND accountId=?"))) 
+	if( ($stmt = $mysqli->prepare("DELETE FROM transactions WHERE userId=? AND accountId=?"))) 
 	{	
 		//bind
 		if(! $stmt->bind_param("ii", $userId, $accountId) )
@@ -18,18 +19,27 @@ function removeAccount($userId, $accountId) {
 		if(! $stmt->execute() ) echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 
 	} else {
-		echo "Prepare failed: (" . $db->errno . ") " . $db->error; //remove after debug
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error; //remove after debug
 	}
 }
 
 //Gets an accountId given input. Creates one if none exists.
 function getAccountId($institution, $type) {
+	global $mysqli;
 	//prepare
-	if( ($stmt = $db->prepare(""
-		. "INSERT INTO accounts (institution, type)"
-		. "SELECT id"
-		. "WHERE NOT EXISTS (SELECT id FROM accounts WHERE institution=? AND type=?)"
-		. "SELECT id FROM accounts WHERE institution=? AND type=?") )) {
+	if( ($stmt = $mysqli->prepare("
+		-- One select
+		SELECT @id = id
+		FROM accounts
+		WHERE institution=? AND type=?
+
+		-- Optionally one insert
+		IF @id IS NULL THEN
+		    INSERT INTO accounts (institution, type)
+		    VALUES (?,?)
+		    SET @id = SCOPE_IDENTITY()
+		END IF"
+		) )) {
 
 		//escape special characters
 		$institution = htmlentities($institution);
@@ -48,7 +58,7 @@ function getAccountId($institution, $type) {
 
 		return $id;
 	} else {
-		echo "Prepare failed: (" . $db->errno . ") " . $db->error; //remove after debug
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error; //remove after debug
 	}
 }
 
@@ -60,18 +70,14 @@ function getAccountIds($userId) {
 //Inserts the transaction into the database.
 //Uses prepared statements and converts chars to htmlentities.
 function insertTransaction($userId, $accountId, $descriptor, $amount, $category, $timestamp) {
+	global $mysqli;
 	//prepare
-	if( ($stmt = $db->prepare("INSERT INTO transactions (userId, accountId, descriptor, amount, category, `timestamp`) VALUES (?,?,?,?,?,?)"))) 
+	if( ($stmt = $mysqli->prepare("INSERT INTO transactions (userId, accountId, descriptor, amount, category, `timestamp`) VALUES (?,?,?,?,?,?)"))) 
 	{
 		
-		//get timestamp
-		$date = new DateTime();
-		$time = $date->getTimestamp();
-
 		//escape special characters
 		$descriptor = htmlentities($descriptor);
 		$category = htmlentities($amount);
-		
 		
 		//bind
 		if(! $stmt->bind_param("iisdsi", $userId, $accountId, $descriptor, $amount, $category, $timestamp) )
@@ -82,17 +88,18 @@ function insertTransaction($userId, $accountId, $descriptor, $amount, $category,
 		
 	}
 	else {
-		echo "Prepare failed: (" . $db->errno . ") " . $db->error; //remove after debug
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error; //remove after debug
 	}
 }
 
 //Gets all transactions tied to $userId "AND" $accountId.
 //Returns an array of transactions.
 function getTransactions($userId, $accountId) {
+	global $mysqli;
 	$ret = array();	//what is to be returned.
 
 	//prepare
-	if( ($stmt = $db->prepare("SELECT id, userId, accountId, descriptor, amount, category, `timestamp` FROM accounts WHERE userId=? AND accountId=?") )) {
+	if( ($stmt = $mysqli->prepare("SELECT id, userId, accountId, descriptor, amount, category, `timestamp` FROM accounts WHERE userId=? AND accountId=?") )) {
 
 		//bind
 		if(! $stmt->bind_param("ii", $userId, $accountId) )
@@ -109,9 +116,12 @@ function getTransactions($userId, $accountId) {
 
 		return $ret;
 	} else {
-		echo "Prepare failed: (" . $db->errno . ") " . $db->error; //remove after debug
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error; //remove after debug
 	}
 }
+
+//Do we need this functionality?
+function getTransactionId() {}
 
 
 ?>
---
+++ test.php
@@ -0,0 +1,34 @@
+<?php
+
+//testing some of the functions
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/db/queries.php";
+
+
+
+$accountId = getAccountId("Bank of America", "Student Loan");
+
+insertTransaction(1, $accountId, "Student loan dispersed", 3000.00, "loan", 0);
+insertTransaction(1, $accountId, "Student loan payment 1", -150.00, "loan", 2400);
+insertTransaction(1, $accountId, "Student loan payment 2", -150.00, "loan", 2700);
+insertTransaction(1, $accountId, "Student loan payment 3", -150.00, "loan", 2900);
+
+$accountId2 = getAccountId("Wells Fargo", "Debit Card");
+
+insertTransaction(1, $accountId2, "Deposit", 3000.00, "card", 9);
+insertTransaction(1, $accountId2, "Gas purchase 1", -60.00, "card", 2409);
+insertTransaction(1, $accountId2, "Food purchase from McDonalds", -32.00, "card", 2709);
+insertTransaction(1, $accountId2, "Gas purchase 2", -150.00, "card", 2909);
+
+$all_transactions = getTransactions(1, $accountId2);
+
+foreach($all_transactions as $transaction) {
+	echo "---<br>";
+	echo $transaction->timestamp;
+	echo " ";
+	echo $transaction->descriptor;
+	echo " ";
+	echo $transaction->amount;	
+	echo " ";
+	echo $transaction->category;
+}
