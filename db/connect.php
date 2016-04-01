01fbb33e2e6825262481232da68e07e090f84dec
Steve Chang <steve.chang.1994@gmail.com>
2016-04-01 15:12:16 -0700

    added transaction model, and database interface

---
+++ db/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$dbname = "minance";
+$username = "dbworker";
+$password = "";	//TODO enter a password here
+
+// Create connection
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+// Check for errors
+if ($mysqli->connect_errno) {
+    echo "Database error: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+?>
