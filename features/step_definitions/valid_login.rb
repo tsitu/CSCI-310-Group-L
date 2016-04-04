5ceb317c4d1df59d71468c074e0cfd7c810aebf3
Zhijie Chen <zhijiech@usc.edu>
2016-04-03 19:39:00 -0700

    added login template cucumber test case

---
+++ features/step_definitions/valid_login.rb
@@ -0,0 +1,13 @@
+Given(/^user is on the login page$/) do
+	expect(page).have_selector('login-username')
+	expect(page).have_selector('login-password')
+end
+
+When(/^user types the right password and username$/) do
+	find('#login-username').set()
+	find('#login-password').set()
+end
+
+Then(/^user can login$/)do
+	expect(current_path).to eq '/'
+end 
