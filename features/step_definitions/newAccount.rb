203cd5be9acf30c214cce3d86d44a8d65f5f9cb2
Zhijie Chen <zhijiech@usc.edu>
2016-04-03 21:09:17 -0700

    fixing cucumber bugs

--- features/step_definitions/newAccount.rb
+++ features/step_definitions/newAccount.rb
@@ -1,13 +1,13 @@
-Given (/^user is on the main UI$/) do
+Given (/^user is on UI$/) do
 	visit('http://localhost')
 end
 
-When(/^user clicks the account button$/)
+When(/^user clicks the account button$/) do
 	if(page.has_button?('add-account'))
 		page.click_button('add-account')
 	end
 end
 
-Then(/^user can create new account)
+Then(/^user can create new account$/) do
 	expect(page).to have_selector('#new-account-diaglog', visible: true)
 end
