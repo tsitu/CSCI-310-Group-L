783013f2cdeace35e654a914bee4ddba92f644a7
Zhijie Chen <zhijiech@usc.edu>
2016-04-13 22:50:40 -0700

    add account test added

---
+++ features/step_definitions/addAccount.rb
@@ -0,0 +1,19 @@
+Given(/^user has an account csv file$/) do 
+	Capybara.reset_sessions!
+	visit("https://localhost/CSCI-310-Group-L/www/login/")
+	find('#login-username').set('test@gmail.com')
+	find('#login-password').set('test')
+	page.execute_script("$('#login-button').click()")
+	list = Array.new
+	list = find('#account-list').all('li')
+	@size = list.size
+end 
+
+When(/^user adds it through browse$/) do 
+	attach_file('#csv-file', File.absolute_path('./test.csv'))
+	page.execute_script("$('.new-account-button').click()")
+end 
+
+Then(/^account list increases$/) do 
+	expect(find('#account-list')).to have_selector('li', count: @size + 1)
+end 
