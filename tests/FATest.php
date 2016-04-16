f7765958b10c4486d34979006d0ea4d8f0ee8e55
Taegyum Kim <hotdog3521@gmail.com>
2016-04-16 14:52:29 -0700

    FAteasing class

---
+++ tests/FATest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/../www/src/inc/queries.php';
+
+class FAtest extends PHPUnit_Framework_TestCase
+{
+
+	protected $backupGlobals = FALSE;
+
+
+//case 1; when empty csv file 
+	public function test2(){
+
+
+
+	
+	}
+	public function test2() {
+
+	}
+	public function test3() {
+
+	}
+}
+
+?> 
