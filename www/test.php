d7d340899850010877cca7f51880689791593a00
Steve Chang <steve.chang.1994@gmail.com>
2016-04-16 19:29:32 -0700

    added accounts

--- www/test.php
+++ www/test.php
@@ -1,50 +1,19 @@
 <?php
 
-//require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/AccountDBManager.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/Account.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/AccountDBManager.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/User.php";
 
-// echo "These are the accounts related to userid 1:<br>";
 
-// $accountdb = new AccountDBManager();
-// $theirAccounts = $accountdb->getAccountsWithBalance(1);
-
-// foreach ($theirAccounts as $account) {
-// 	echo $account->institution . " - " . $account->type . " --- $" . $account->balance . "<br>";
-// }
-
-// echo "<br>";
-
-
-
-// echo "These are the last 3 transactions for userid 1:<br>";
-
-// $transactiondb = new TransactionDBManager();
-// $theirTransactions = $transactiondb->getTransactionsForUser(1);
-
-// foreach ($theirTransactions as $transaction) {
-// 	echo $transaction->category . " - " . $transaction->descriptor . " --- $" . $transaction->amount . "<br>";
-// }
-
-
-// $account = new Account(1, "Bank of America", "Savings");
-// echo "<br>";
-// $account2 = new Account(1, "Chase", "Savings");
-
-// echo "<br>done.";
-
-
-echo "Attempting login...<br>";
-if(User::validateUser("test@gmail.com", "test")) {
-	echo $user->email . " logged in.<br>";
-} else {
-	echo "Login failed.<br>";
-}
-
-// echo "Registering new user...<br>";
-// $user = new User("test@gmail.com", "test");
-
-// echo "User information...<br>";
-// echo "name: " . $user->email . " id: " . $user->id . "pass: " . $user->hashed_password . " encpass: " . DBManager::encrypt($user->hashed_password);
-
-echo "<br>";
+//Account functions
+$account_db = AccountDBManager::getAccountDBManager();
+$account_db->addAccount(1, "Chase", "loan", 2261);
+$account_db->addAccount(2, "Wells Fargo", "loan", 2261);
+$account_db->addAccount(3, "Chase3", "loan", 2261);
+$account_db->addAccount(4, "Chase4", "loan", 2261);
+$account_db->deleteAccount(1);
+$account_db->updateAccount(2, "Chase2", "loan", 2261);
+$list = $account_db->getAllAccounts(2261);
+$x = $account_db->getAccountByInfo("Chase2", "loan", 2261);
+$x = $account_db->getAccountByInfo("Chase2", "loan", 22612);
+if(is_null($x)) echo "tests done successfully<br>";
+else echo "something wrong with AccountDBManager<br>";
