c85150b7aff07395ba745d092c6fe1d6b42daa56
Zhijie Chen <zhijiech@usc.edu>
2016-04-17 21:26:44 -0700

    removed merge syntax error

--- features/step_definitions/newAccount.rb
+++ features/step_definitions/newAccount.rb
@@ -1,16 +1,13 @@
 Given (/^user is on UI$/) do
-<<<<<<< HEAD
 	visit("http://localhost:80")
 	find('#login-username').set('test@gmail.com')
 	find('#login-password').set('test')
 	page.execute_script("$('button.login-button').click()")
-=======
 	Capybara.reset_sessions!
 	visit("https://localhost/CSCI-310-Group-L/www/login/")
 	find('#login-username').set('test@gmail.com')
 	find('#login-password').set('test')
 	page.execute_script("$('#login-button').click()")
->>>>>>> eaf9b4e620b665761a05e90f3e05555f3efa2edc
 end
 
 When(/^user clicks the account button$/) do
@@ -20,9 +17,6 @@ When(/^user clicks the account button$/) do
 end
 
 Then(/^user can create new account$/) do
-<<<<<<< HEAD
 	expect(page).to have_selector('#new-account-diaglog', visible: true)
-=======
 	expect(page).to have_selector('#new-account-dialog', visible: true)
->>>>>>> eaf9b4e620b665761a05e90f3e05555f3efa2edc
 end
--- features/step_definitions/valid_login.rb
+++ features/step_definitions/valid_login.rb
@@ -1,26 +1,20 @@
 Given(/^user is on the login page$/) do
-<<<<<<< HEAD
 	visit("http://localhost:80")
-=======
 	Capybara.reset_sessions!
 	visit("https://localhost/CSCI-310-Group-L/www/login/")
->>>>>>> eaf9b4e620b665761a05e90f3e05555f3efa2edc
 end
 
 When(/^user types the right password and username$/) do
 	find('#login-username').set('test@gmail.com')
 	find('#login-password').set('test')
-<<<<<<< HEAD
 	page.execute_script("$('button.login-button').click()")
 end
 
 Then(/^user can login$/)do
 	expect(current_path).to eq '/'
-=======
 	page.execute_script("$('#login-button').click()")
 end
 
 Then(/^user can login$/)do
 	expect(current_path).to eq '/CSCI-310-Group-L/www/'
->>>>>>> eaf9b4e620b665761a05e90f3e05555f3efa2edc
 end 
