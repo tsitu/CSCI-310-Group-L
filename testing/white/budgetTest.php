092be1e4ce79e1d728f3aee1fc5fa10716d78f68
Taegyum Kim <hotdog3521@gmail.com>
2016-04-24 16:20:35 -0700

    php unit test

--- testing/white/budgetTest.php
+++ testing/white/budgetTest.php
@@ -1,11 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../www/src/inc/queries.php';
+require_once __DIR__ . '/../../www/src/inc/queries.php';
 
 class budgetTest extends PHPUnit_Framework_TestCase
 {
 
 	protected $backupGlobals = FALSE;
+	protected $return_id;
+	protected $return_id2;
 
 
 	function __construct(){
@@ -17,36 +19,40 @@ class budgetTest extends PHPUnit_Framework_TestCase
 	}
 
 	public function testAddBudget() {
-			$before_rowNumber = getNumberOfRowsBudget();
-			BudgetManager::addBudget(500, 12, 2500, 100); 
-			$after_rowNumber = getNumberOfRowsBudget();
-			$this->assertEquals($before_rowNumber+1, $after_rowNumber);
+		$before_rowNumber = getNumberOfRowsBudget();
+		$return_id = BudgetManager::addBudget(500, 12, 2500, 100); 
+		$after_rowNumber = getNumberOfRowsBudget();
+		$this->assertEquals($return_id, 500);
+		$this->assertEquals($before_rowNumber+1, $after_rowNumber);
 
 	}
-	public function testValidUserId() {
+	public function testExistId() {
+		$before_rowNumber = getNumberOfRowsBudget();
+		$return_id2 = BudgetManager::addBudget(500, 12, 2500, 100);
+		$after_rowNumber = getNumberOfRowsBudget();
+		$this->assertEquals($return_id2, null);
+		$this->assertEquals($before_rowNumber, $after_rowNumber);
 
 	}
-	public function testValidMonth() {
 
-	}
-	public function testValidbudget() {
-
-	}
 	public function testDeleteBudget() {
-			$before_rowNumber = getNumberOfRowsBudget();
-			BudgetManager::deleteBudget(500); 
-			$after_rowNumber = getNumberOfRowsBudget();
-			$this->assertEquals($before_rowNumber-1, $after_rowNumber);
+		$before_rowNumber = getNumberOfRowsBudget();
+		BudgetManager::deleteBudget(500); 
+		$after_rowNumber = getNumberOfRowsBudget();
+		$this->assertEquals($before_rowNumber-1, $after_rowNumber);
 
-	}
-	public function testValidIdInDeleteBudget() {
-		
 	}
 	public function testGetBudgetByInfo(){
-
+		$return_budget = BudgetManager::getBudgetByInfo(520,12,2500);
+		$this->asserEquals($return_budget, null);
 	}
 	public function testGetBudgetsByUser(){
-
+		$return_budgetArray = BudgetManager::getBudgetByUser(500);
+		$this->assertEquals(sizeof($return_budgetArray), 1);
+	}
+	public function testEmptyBudget(){
+		$return_budgetArray = BudgetManager::getBudgetByUser(505);
+		$this->assertEquals(sizeof($return_budgetArray), 0);
 	}
 
 }
